400faf080ffde1af36c14bca490d17d9ff9b36b8
DiottoDev <nicodiottodev@gmail.com>
2025-07-24 07:35:59 -0300

    fix: remove console.log

--- backend/src/modules/auth/auth.guard.ts
+++ backend/src/modules/auth/auth.guard.ts
@@ -18,7 +18,6 @@ export class AuthGuard implements CanActivate {
       throw new UnauthorizedException();
     }
     try {
-      console.log('Verifying token:', token);
       const payload = await this.jwtService.verifyAsync<{
         sub: string;
         email: string;
