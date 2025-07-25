94d252c23d65b1a8f4806ebdaec1853397794b5a
DiottoDev <nicodiottodev@gmail.com>
2025-07-25 02:37:28 -0300

    fix: fix validation schema

--- frontend/src/schemas/index.ts
+++ frontend/src/schemas/index.ts
@@ -3,22 +3,18 @@ import z from "zod";
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 export const loginSchema = z.object({
-  email: z
-    .string()
-    .min(1, "Email é obrigatório")
-    .regex(emailRegex, "Email inválido"),
+  email: z.string("Email é obrigatório").regex(emailRegex, "Email inválido"),
   password: z.string().min(8, "A senha deve ter pelo menos 8 caracteres"),
 });
 
 export type LoginFormData = z.infer<typeof loginSchema>;
 
 export const registerSchema = z.object({
-  name: z.string().min(1, "Nome é obrigatório"),
-  email: z
-    .string()
-    .min(1, "Email é obrigatório")
-    .regex(emailRegex, "Email inválido"),
-  password: z.string().min(8, "A senha deve ter pelo menos 8 caracteres"),
+  name: z.string("Nome é obrigatório"),
+  email: z.string("Email é obrigatório").regex(emailRegex, "Email inválido"),
+  password: z
+    .string("Senha é obrigatória")
+    .min(8, "A senha deve ter pelo menos 8 caracteres"),
 });
 
 export type RegisterFormData = z.infer<typeof registerSchema>;
@@ -45,12 +41,12 @@ export type Tasks = {
 };
 
 export const taskSchema = z.object({
-  title: z.string().min(1, "Título é obrigatório"),
+  title: z.string("Título é obrigatório"),
   type: z.enum(TaskType),
 });
 
 export const updateTaskSchema = taskSchema.extend({
-  id: z.string().min(1, "ID é obrigatório"),
+  id: z.string("ID é obrigatório"),
 });
 
 export type UpdateTaskFormData = z.infer<typeof updateTaskSchema>;
